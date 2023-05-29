74302dabea81935cb396f209f804aab1a52e0712
Undeadamien <126392901+Undeadamien@users.noreply.github.com>
2023-05-29 17:09:57 +0200

    code cleanning and addition of CLI

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import sys
 
 
-def plot_reprint(df: pd.DataFrame):
+def plot_reprint() -> None:
     """
     Plot a pie, showing the percentage of cards who are reprint of others cards
     """
 
-    df = df.dropna(subset=["artist", "name", "isReprint"])
-
+    df = pd.read_csv("./cards.csv", usecols=["artist", "name", "isReprint"])
     total_print = df.shape[0]
-    df = df["isReprint"].value_counts()
-    df = df.rename({0: "original card", 1: "reprint"})
+    df = df["isReprint"].value_counts().rename({0: "original card", 1: "reprint"})
 
-    plot = df.plot(
+    df.plot(
         kind="pie",
         autopct="%1.1f%%",
         ylabel="",
@@ -23,25 +22,20 @@ def plot_reprint(df: pd.DataFrame):
     plt.show()
 
 
-def plot_artist(df: pd.DataFrame):
+def plot_artist() -> None:
     """
     Plot the number of card by each illustrated by each artist,
     seperate the cards illustrated by one artist and the cards illustrated
-    by a duo of artist.
+    by a duo of artist. Some artist are in multiple duo.
     """
 
-    df = df.dropna(subset=["artist", "name", "isReprint"])
-
+    df = pd.read_csv("./cards.csv", usecols=["artist", "name", "isReprint"]).dropna()
     df["mask"] = df["artist"].str.contains("&")
-
-    # remove the two sided cards
     df = df[["artist", "name", "mask"]].drop_duplicates()
 
-    # seperate the solo and duo illustration
     df_solo = df[~df["mask"]].value_counts("artist", ascending=True)
     df_duo = df[df["mask"]].value_counts("artist", ascending=True)
 
-    # start plotting
     fig, axes = plt.subplots(nrows=2)
     fig.suptitle("Number of cards illustrated per artist")
 
@@ -58,16 +52,13 @@ def plot_artist(df: pd.DataFrame):
     plt.show()
 
 
-def plot_mana_cost(df: pd.DataFrame):
+def plot_mana_cost() -> None:
     """
     Plot the cards by their mana cost
     """
 
-    df = (
-        df.drop_duplicates(subset=["name", "manaValue"])
-        .value_counts("manaValue")
-        .sort_index()
-    )
+    df = pd.read_csv("./cards.csv", usecols=["name", "manaValue"])
+    df = df.drop_duplicates().value_counts("manaValue").sort_index()
 
     plot = df.plot(kind="bar", rot=0)
     plot.bar_label(plot.containers[0])
@@ -78,12 +69,56 @@ def plot_mana_cost(df: pd.DataFrame):
     plt.show()
 
 
-def main():
-    df = pd.read_csv("./cards.csv")
+def plot_most_reprinted_cards() -> None:
+    """
+    Plot the most reprinted cards excluding the lands
+    """
 
-    plot_mana_cost(df)
-    plot_reprint(df)
-    plot_artist(df)
+    df = pd.read_csv("./cards.csv", usecols=["name", "types"])
+    df = df.loc[(df["types"] != "Land")]  # exclude the lands
+    df = df["name"].value_counts()[:10]  # select the top ten
+
+    plot = df.plot(kind="bar", rot=0)
+    plot.bar_label(plot.containers[0])
+    plot.tick_params("y", which="both", left=False, labelleft=False)
+    plot.set_title("Most reprinted cards (excluding lands)")
+    plot.set_xlabel("")
+    plt.show()
+
+
+def print_choice():
+    print("What do you want to see:")
+    print("1 : Artist participation")
+    print("2 : Mana cost")
+    print("3 : Ten most reprinted cards")
+    print("4 : Percentage of cards reprinted")
+    print("5 : Quit")
+
+
+def main():
+    while True:
+        print_choice()
+        input_ = input().strip()
+
+        match input_:
+            case "1":
+                return plot_artist()
+
+            case "2":
+                return plot_mana_cost()
+
+            case "3":
+                return plot_most_reprinted_cards()
+
+            case "4":
+                return plot_reprint()
+
+            case "5":
+                sys.exit()
+
+            case _:
+                print("Invalid input\n")
+                continue
 
 
 if __name__ == "__main__":
